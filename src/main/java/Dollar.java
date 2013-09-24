d98a66776b6c3cc53f6b3eb1b7a75f5e9c11b274
cherish <shmilycherish@gmail.com>
2013-09-24 22:26:35 +0800

    refactor someplace

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -1,11 +1,11 @@
 public class Dollar {
     public int amount;
 
-    public Dollar(int i) {
-
+    public Dollar(int amount) {
+        this.amount = amount;
     }
 
-    public void times(int i) {
-
+    public void times(int multiplier) {
+        amount *= multiplier;
     }
 }
