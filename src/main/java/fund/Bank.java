bf7aae7fb7a4c29769e6cf44171316d1f3f9c5c9
cherish <shmilycherish@gmail.com>
2013-10-15 21:45:58 +0800

    add ratetable

--- src/main/java/fund/Bank.java
+++ src/main/java/fund/Bank.java
@@ -1,15 +1,20 @@
 package fund;
 
+import java.util.Hashtable;
+
 public class Bank {
+    private Hashtable rates = new Hashtable();
+
     public Money reduce(Expression source, String to) {
         return source.reduce(this, to);
     }
 
-    public void addRate(String chf, String usd, int i) {
-        //To change body of created methods use File | Settings | File Templates.
+    public void addRate(String from, String to, int rate) {
+        rates.put(new Pair(from, to), rate);
     }
 
     public int rate(String from, String to) {
-        return (from.equals("CHF") && to.equals("USD")) ? 2 :1;
+        if(from.equals(to)) return 1;
+        return (Integer)rates.get(new Pair(from, to));
     }
 }
