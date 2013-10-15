587c114e8043fb820b6752837f8ff5ecb0144e47
cherish <shmilycherish@gmail.com>
2013-10-15 21:35:30 +0800

    add rate

--- src/main/java/fund/Bank.java
+++ src/main/java/fund/Bank.java
@@ -2,6 +2,14 @@ package fund;
 
 public class Bank {
     public Money reduce(Expression source, String to) {
-        return source.reduce(to);
+        return source.reduce(this, to);
+    }
+
+    public void addRate(String chf, String usd, int i) {
+        //To change body of created methods use File | Settings | File Templates.
+    }
+
+    public int rate(String from, String to) {
+        return (from.equals("CHF") && to.equals("USD")) ? 2 :1;
     }
 }
--- src/test/java/MultiFundTest.java
+++ src/test/java/MultiFundTest.java
@@ -71,5 +71,13 @@ public class MultiFundTest {
         Money result = bank.reduce(Money.dollar(1), "USD");
         assertEquals(Money.dollar(1), result);
     }
+
+    @Test
+    public void reduceDifferentCurrencyTest() throws Exception {
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(Money.franc(2), "USD");
+        assertEquals(Money.dollar(1), result);
+    }
 }
 
